870dbfbc8c02d4e88019261ea9a03d913a9ca413
Timothy Flowers <t_chuck_flowers@yahoo.com>
2018-03-02 21:33:40 -0500

    Sets up a basic version of the command line argument parser

---
+++ src/TestSketch/IO/Shell/CommandLineOptions.cs
@@ -0,0 +1,10 @@
+﻿using CommandLine;
+
+namespace TestSketch.IO.Shell
+{
+    internal class CommandLineOptions
+    {
+        [Option('c', "config", Required = true, HelpText = "The file that contains the configuration for how the tests are generated")]
+        public string ConfigFile { get; set; }
+    }
+}
---
+++ src/TestSketch/IO/Shell/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using CommandLine;
+
+namespace TestSketch.IO.Shell
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Parser.Default
+                .ParseArguments<CommandLineOptions>(args)
+                .WithParsed(Run)
+                .WithNotParsed(HandleParseError);
+        }
+
+        private static void HandleParseError(IEnumerable<Error> errors)
+        {
+            foreach (Error error in errors)
+                Console.WriteLine("There was an error: " + error);
+        }
+
+        private static void Run(CommandLineOptions options)
+        {
+            Console.WriteLine("ConfigFile = " + options.ConfigFile);
+        }
+    }
+}
