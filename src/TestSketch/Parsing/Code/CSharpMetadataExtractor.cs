40dbc409f6d2f7223b3aec846ee0608fb98e25a1
Timothy Flowers <t_chuck_flowers@yahoo.com>
2018-03-03 02:16:59 -0500

    Creates the logic that can parse a C# file and extract the pertinent metadata

---
+++ src/TestSketch/Parsing/Code/CSharpMetadataExtractor.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using TestSketch.IO.Files.Metadata;
+
+namespace TestSketch.Parsing.Code
+{
+    internal class CSharpMetadataExtractor : IMetadataExtractor
+    {
+        public IEnumerable<TypeMetadata> ExtractedMetadata => walker.ExtractedMetadata;
+
+        public void ProcessCodeFile(string path)
+        {
+            var root = CSharpSyntaxTree.ParseText(File.ReadAllText(path)).GetRoot();
+            walker.Visit(root);
+        }
+
+        private class CSharpMetadataWalker : CSharpSyntaxWalker
+        {
+            public LinkedList<TypeMetadata> ExtractedMetadata { get; } = new LinkedList<TypeMetadata>();
+
+            public override void VisitClassDeclaration(ClassDeclarationSyntax node) => ExtractTypeMetadata(node);
+
+            public override void VisitStructDeclaration(StructDeclarationSyntax node) => ExtractTypeMetadata(node);
+
+            private MethodMetadata ExtractMethodMetadata(MethodDeclarationSyntax node)
+            {
+                var methodName = node.Identifier.Text;
+                return new MethodMetadata(methodName);
+            }
+
+            private void ExtractTypeMetadata(TypeDeclarationSyntax node)
+            {
+                var typeName = node.Identifier.Text;
+                var methodData = new LinkedList<MethodMetadata>();
+
+                foreach (MethodDeclarationSyntax methodNode in node.Members.Where(m => m is MethodDeclarationSyntax))
+                    methodData.AddLast(ExtractMethodMetadata(methodNode));
+
+                ExtractedMetadata.AddLast(new TypeMetadata(typeName, methodData));
+            }
+        }
+
+        private CSharpMetadataWalker walker = new CSharpMetadataWalker();
+    }
+}
---
+++ src/TestSketch/Parsing/Code/MetadataExtractorFactory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace TestSketch.Parsing.Code
+{
+    internal static class MetadataExtractorFactory
+    {
+        public static IMetadataExtractor GetExtractor(string extension)
+        {
+            if (CSharpExtensions.Contains(extension))
+                return new CSharpMetadataExtractor();
+            else if (VisualBasicExtensions.Contains(extension))
+                throw new Exception($"Visual Basic is not yet supported by {nameof(TestSketch)}");
+
+            throw new Exception($"The extension '{extension}' was unrecognized.");
+        }
+
+        private static readonly Collection<string> CSharpExtensions = new Collection<string>()
+        {
+            ".cs"
+        };
+
+        private static readonly Collection<string> VisualBasicExtensions = new Collection<string>()
+        {
+            ".vb"
+        };
+    }
+}
