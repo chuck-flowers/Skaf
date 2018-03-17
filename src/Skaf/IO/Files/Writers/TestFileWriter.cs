4f1c3add0c13f2211824a1157254a0a67e41d8ea
Timothy Flowers <t_chuck_flowers@yahoo.com>
2018-03-17 01:51:16 -0400

    Adds support for iterative runs of Skaf so that previously written tests are no longer lost

--- src/Skaf/IO/Files/Writers/TestFileWriter.cs
+++ src/Skaf/IO/Files/Writers/TestFileWriter.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.IO;
-using System.Runtime.CompilerServices;
+using System.Linq;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
@@ -26,7 +26,11 @@ namespace Skaf.IO.Files.Writers
         public void Write()
         {
             //Create the structure
-            var compilationUnit = CreateCompilationUnit();
+            CompilationUnitSyntax compilationUnit = null;
+            if (File.Exists(TestFile.Path))
+                compilationUnit = (CompilationUnitSyntax)CSharpSyntaxTree.ParseText(File.ReadAllText(TestFile.Path)).GetRoot();
+
+            compilationUnit = CreateCompilationUnit(compilationUnit);
 
             //Write the structure
             var formatted = Formatter.Format(compilationUnit, new AdhocWorkspace());
@@ -35,52 +39,147 @@ namespace Skaf.IO.Files.Writers
                 formatted.WriteTo(writer);
         }
 
-        private CompilationUnitSyntax CreateCompilationUnit() =>
-            CompilationUnit()
-                .AddUsings(
-                    UsingDirective(IdentifierName("System")),
-                    UsingDirective(IdentifierName("System.Collections")),
-                    UsingDirective(IdentifierName("System.Collections.Generic")),
-                    UsingDirective(IdentifierName("Xunit"))
-                ).AddMembers(
-                    CreateNameSpaceSyntax()
-                );
-
-        [MethodImpl(MethodCodeType = MethodCodeType.IL)]
-        private MethodDeclarationSyntax CreateMethodSyntax(MethodMetadata methodMetadata) =>
-            MethodDeclaration(PredefinedType(Token(SyntaxKind.VoidKeyword)), Identifier(methodMetadata.Name + "Test"))
-                .AddAttributeLists(
-                    AttributeList(
-                        SingletonSeparatedList(
-                            Attribute(IdentifierName("Fact"))
-                        )
-                    )
-                )
-                .AddBodyStatements(CreateThrowStatementSyntax());
-
-        private NamespaceDeclarationSyntax CreateNameSpaceSyntax() =>
-            NamespaceDeclaration(IdentifierName(Type.Namespace))
-                .AddMembers(CreateTestClass());
-
-        private ClassDeclarationSyntax CreateTestClass()
+        private CompilationUnitSyntax CreateCompilationUnit(CompilationUnitSyntax compilationUnit)
         {
-            object o = new object();
-            var c = ClassDeclaration(Type.Name + "Tests")
-                .AddModifiers(Token(SyntaxKind.PublicKeyword));
+            if (compilationUnit == null)
+                compilationUnit = CompilationUnit();
 
-            foreach (var m in Type.Methods)
-                c = c.AddMembers(CreateMethodSyntax(m));
+            var oldUsings = compilationUnit.Usings;
+            var newUsings = oldUsings;
 
-            return c;
+            bool uSystem, uSystemCollections, uSystemCollectionsGeneric, uXunit;
+            uSystem = uSystemCollections = uSystemCollectionsGeneric = uXunit = false;
+
+            //Detect usings
+            foreach (var u in oldUsings)
+            {
+                var ns = u.Name.GetText().ToString();
+
+                switch (ns)
+                {
+                    case "System":
+                        uSystem = true;
+                        break;
+
+                    case "System.Collections":
+                        uSystemCollections = true;
+                        break;
+
+                    case "System.Collections.Generics":
+                        uSystemCollectionsGeneric = true;
+                        break;
+
+                    case "Xunit":
+                        uXunit = true;
+                        break;
+                }
+            }
+
+            //Create missing usings
+            if (!uSystem)
+            {
+                newUsings = newUsings.Add(
+                    UsingDirective(IdentifierName("System"))
+                );
+            }
+
+            if (!uSystemCollections)
+            {
+                newUsings = newUsings.Add(
+                    UsingDirective(IdentifierName("System.Collections"))
+                );
+            }
+
+            if (!uSystemCollectionsGeneric)
+            {
+                newUsings = newUsings.Add(
+                    UsingDirective(IdentifierName("System.Collections.Generic"))
+                );
+            }
+
+            if (!uXunit)
+            {
+                newUsings = newUsings.Add(
+                    UsingDirective(IdentifierName("Xunit"))
+                );
+            }
+
+            var oldMembers = compilationUnit.Members;
+            var oldNsDeclaration = oldMembers
+                .OfType<NamespaceDeclarationSyntax>()
+                .FirstOrDefault();
+            var newNsDeclaration = CreateNameSpaceSyntax(oldNsDeclaration);
+
+            var newMembers = oldNsDeclaration != null ?
+                oldMembers.Replace(oldNsDeclaration, newNsDeclaration) :
+                oldMembers.Add(newNsDeclaration);
+
+            return compilationUnit
+                .WithUsings(newUsings)
+                .WithMembers(newMembers);
         }
 
-        private ThrowStatementSyntax CreateThrowStatementSyntax() =>
-            ThrowStatement(
+        private MethodDeclarationSyntax CreateMethodSyntax(MethodMetadata methodMetadata)
+        {
+            return MethodDeclaration(
+                PredefinedType(Token(SyntaxKind.VoidKeyword)),
+                Identifier(methodMetadata.Name + "Test"))
+                    .AddAttributeLists(
+                        AttributeList(
+                            SingletonSeparatedList(
+                                Attribute(IdentifierName("Fact"))
+                            )
+                        )
+                    ).AddBodyStatements(
+                        CreateThrowStatementSyntax()
+                ).WithModifiers(TokenList(Token(SyntaxKind.PublicKeyword)));
+        }
+
+        private NamespaceDeclarationSyntax CreateNameSpaceSyntax(NamespaceDeclarationSyntax namespaceDeclaration)
+        {
+            if (namespaceDeclaration == null)
+                namespaceDeclaration = NamespaceDeclaration(IdentifierName(Type.Namespace));
+
+            var oldMembers = namespaceDeclaration.Members;
+            var oldClass = oldMembers
+                .OfType<ClassDeclarationSyntax>()
+                .FirstOrDefault();
+            var newClass = CreateTestClass(oldClass);
+
+            var newMembers = oldClass != null ?
+                oldMembers.Replace(oldClass, newClass) :
+                oldMembers.Add(newClass);
+
+            return namespaceDeclaration.WithMembers(newMembers);
+        }
+
+        private ClassDeclarationSyntax CreateTestClass(ClassDeclarationSyntax classDeclaration)
+        {
+            if (classDeclaration == null)
+                classDeclaration = ClassDeclaration(Type.Name + "Tests")
+                    .WithModifiers(TokenList(Token(SyntaxKind.PublicKeyword)));
+
+            foreach (var m in Type.Methods)
+            {
+                if (classDeclaration.Members.OfType<MethodDeclarationSyntax>().Any(d => d.Identifier.Text.Equals(m.Name + "Test")))
+                    continue;
+                else
+                    classDeclaration = classDeclaration.AddMembers(CreateMethodSyntax(m));
+            }
+
+            return classDeclaration;
+        }
+
+        private ThrowStatementSyntax CreateThrowStatementSyntax()
+        {
+            return ThrowStatement(
                 ObjectCreationExpression(
-                    IdentifierName(nameof(NotImplementedException))
-                ).WithArgumentList(
-                    ArgumentList()
-                )
-            );
+                    IdentifierName(
+                        nameof(NotImplementedException))
+                    ).WithArgumentList(
+                        ArgumentList()
+                    )
+                );
+        }
     }
 }
