8a985c8f362f572301e058385acb17a1c4ecaa49
Timothy Flowers <t_chuck_flowers@yahoo.com>
2018-03-19 23:18:38 -0400

    Factors the map phase out into its own class.

---
+++ src/Skaf/Orchestration/Map/MapPhase.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Skaf.IO.Files;
+using Skaf.IO.Files.Mapping;
+using Skaf.IO.Files.Metadata;
+
+namespace Skaf.Orchestration.Map
+{
+    public class MapPhase
+    {
+        public MapPhase(IEnumerable<TypeMetadata> types)
+        {
+            Types = types;
+        }
+
+        public IEnumerable<TypeMetadata> Types { get; }
+
+        public IEnumerable<(TypeMetadata, TestFile)> Execute()
+        {
+            var testProjectRoot = Path.Combine("..", "..", "test");
+            testProjectRoot = Path.GetFullPath(testProjectRoot);
+            TestFileMapper testFileMapper = new TestFileMapper(testProjectRoot);
+
+            return Types.Select(t => (t, testFileMapper.MapTypeToTestFile(t)));
+        }
+    }
+}
--- src/Skaf/Orchestration/ProcessOrchestrator.cs
+++ src/Skaf/Orchestration/ProcessOrchestrator.cs
@@ -1,12 +1,7 @@
-﻿using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using Skaf.IO.Config;
-using Skaf.IO.Files;
-using Skaf.IO.Files.Mapping;
-using Skaf.IO.Files.Metadata;
+﻿using Skaf.IO.Config;
 using Skaf.IO.Files.Writers;
 using Skaf.Orchestration.Input;
+using Skaf.Orchestration.Map;
 
 namespace Skaf.Orchestration
 {
@@ -21,7 +16,7 @@ namespace Skaf.Orchestration
         public void Execute()
         {
             var inputTypes = new InputPhase(BaseDirectory, Configuration.InputConfig).Execute();
-            var mappings = MapToTestFiles(inputTypes);
+            var mappings = new MapPhase(inputTypes).Execute();
             foreach (var pair in mappings)
             {
                 (var type, var testFile) = pair;
@@ -33,19 +28,5 @@ namespace Skaf.Orchestration
         private string BaseDirectory { get; }
 
         private Configuration Configuration { get; }
-
-        /// <summary>
-        /// Associates each of the input types with a test file
-        /// </summary>
-        /// <param name="types">The types to associate with test files</param>
-        /// <returns>The pairing of type metadata with its appropriate test file</returns>
-        private IEnumerable<(TypeMetadata, TestFile)> MapToTestFiles(IEnumerable<TypeMetadata> types)
-        {
-            var testProjectRoot = Path.Combine("..", "..", "test");
-            testProjectRoot = Path.GetFullPath(testProjectRoot);
-            TestFileMapper testFileMapper = new TestFileMapper(testProjectRoot);
-
-            return types.Select(t => (t, testFileMapper.MapTypeToTestFile(t)));
-        }
     }
 }
