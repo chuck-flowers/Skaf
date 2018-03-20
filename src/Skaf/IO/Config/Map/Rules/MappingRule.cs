64e3d8ff45b4a336dbb660fa8f847855681569a6
Timothy Flowers <t_chuck_flowers@yahoo.com>
2018-03-20 00:33:20 -0400

    Scaffolds the map phase configuration classes.

---
+++ src/Skaf/IO/Config/Map/Rules/MappingRule.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace Skaf.IO.Config.Map.Rules
+{
+    public class MappingRule
+    {
+        [JsonProperty("input")]
+        public TypeMatcher Input { get; set; }
+
+        [JsonProperty("output")]
+        public TestGenerator Output { get; set; }
+    }
+}
---
+++ src/Skaf/IO/Config/Map/Rules/TestGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Newtonsoft.Json;
+using Skaf.IO.SourceCode.Metadata;
+
+namespace Skaf.IO.Config.Map.Rules
+{
+    public class TestGenerator
+    {
+        [JsonProperty("method")]
+        public string Method { get; set; }
+
+        [JsonProperty("namespace")]
+        public string Namespace { get; set; }
+
+        [JsonProperty("path")]
+        public string Path { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        public MethodMetadata GenerateTest()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
