2179b8fcae9ee0602d8b7404dee9d57a72cff97f
Timothy Flowers <t_chuck_flowers@yahoo.com>
2019-09-11 19:12:24 -0400

    Adds doc comments to the MapPhase class

--- src/Skaf/Orchestration/Map/MapPhase.cs
+++ src/Skaf/Orchestration/Map/MapPhase.cs
@@ -7,26 +7,54 @@ using Skaf.IO.SourceCode.Metadata;
 
 namespace Skaf.Orchestration.Map
 {
+    /// <summary>
+    /// The phase of the process that is responsible for mapping the methods within a source file to
+    /// a test method within a test file.
+    /// </summary>
     public class MapPhase
     {
-        public MapPhase(IEnumerable<MethodMetadata> types, MapConfiguration mapConfig)
+        /// <summary>
+        /// Creates the a <see cref="MapPhase"/> that maps a set of <paramref name="srcMethods"/>.
+        /// </summary>
+        /// <param name="srcMethods">The source methods for which to generate test methods.</param>
+        /// <param name="mapConfig">
+        /// The configuration that specifies how to transform the source methods into the test methods.
+        /// </param>
+        public MapPhase(IEnumerable<MethodMetadata> srcMethods, MapConfiguration mapConfig)
         {
             MapConfig = mapConfig ?? throw new ArgumentNullException(nameof(mapConfig));
-            Types = types ?? throw new ArgumentNullException(nameof(types));
+            SourceMethods = srcMethods ?? throw new ArgumentNullException(nameof(srcMethods));
         }
 
+        /// <summary>
+        /// The configuration specifying how to transform the source methods into test methods.
+        /// </summary>
         public MapConfiguration MapConfig { get; }
 
-        public IEnumerable<MethodMetadata> Types { get; }
+        /// <summary>
+        /// The source methods to transform into test methods.
+        /// </summary>
+        public IEnumerable<MethodMetadata> SourceMethods { get; }
 
-        public IEnumerable<(MethodMetadata, MethodMetadata)> Execute()
+        /// <summary>
+        /// Executes the mapping operation for the methods.
+        /// </summary>
+        /// <returns>
+        /// A tuple that contains the source method and the test method that it is mapped to.
+        /// </returns>
+        public IEnumerable<(MethodMetadata srcMethod, MethodMetadata testMethod)> Execute()
         {
             var testProjectRoot = Path.Combine("..", "..", "test");
             testProjectRoot = Path.GetFullPath(testProjectRoot);
 
-            return Types.Select(m => (m, MakeTestMethod(m)));
+            return SourceMethods.Select(m => (m, MakeTestMethod(m)));
         }
 
+        /// <summary>
+        /// Generates a test method for a given source method.
+        /// </summary>
+        /// <param name="sourceMethod">The source method for which to generate the test method.</param>
+        /// <returns>The generated test method.</returns>
         private MethodMetadata MakeTestMethod(MethodMetadata sourceMethod)
         {
             foreach (var rule in MapConfig.MappingRules)
